0cfb112c706264a66848ae7db361c128ec4ea988
Tomas Kraus <tomas.kraus.dev@gmail.com>
2018-09-27 01:25:54 +0200

    add gamepad code with XInput dlls, add ini file related code, make the app run

--- FPVRemote/MainWindow.xaml.cs
+++ FPVRemote/MainWindow.xaml.cs
@@ -13,6 +13,13 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using System.Windows.Threading;
+using IniParser;
+using IniParser.Model;
+
+using FPVRemote.Joyinput;
+
+
 namespace FPVRemote
 {
     /// <summary>
@@ -20,9 +27,63 @@ namespace FPVRemote
     /// </summary>
     public partial class MainWindow : Window
     {
+        private DispatcherTimer _inputCheckTimer;
+
+        IJoyInput ji;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            Loaded += MainWindow_Loaded;
+        }
+
+
+        void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            // MessageBox.Show("Loaded");
+
+            var Parser = new FileIniDataParser();
+            IniData data = Parser.ReadFile("config.ini");
+
+            // ji = new CountJoyInput().InitFromConfig(data, "COUNTJOY2")
+            ji = new GamePadInput()
+                .Chain(new LimitJoyInput(3500));
+
+
+
+
+
+            StartNewInputCheckTimer();
+        }
+
+        private void StartNewInputCheckTimer()
+        {
+            _inputCheckTimer = new DispatcherTimer
+            {
+                Interval = new TimeSpan(0, 0, 0, 0, 50),
+                IsEnabled = true
+            };
+
+            _inputCheckTimer.Tick += InputCheckTimerOnTick;
+            _inputCheckTimer.Start();
+        }
+
+        private void InputCheckTimerOnTick(object sender, EventArgs eventArgs)
+        {
+            ji.Poll();
+            XAxisTextBox.Text = ji.Values.x.ToString();
+        }
+
+
+        private void XAxisTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void XAxisTextBox_TextChanged_1(object sender, TextChangedEventArgs e)
+        {
+
         }
     }
 }
---
+++ FPVRemote/src/Joyinput/GamePadInput.cs
@@ -0,0 +1,16 @@
+﻿using XInputDotNetPure;
+
+namespace FPVRemote.Joyinput
+{
+    class GamePadInput : AJoyInput
+    {
+        private GamePadState state;
+
+        protected override void PollImpl()
+        {
+            state = GamePad.GetState(PlayerIndex.One);
+
+            vals.x = (int)(state.ThumbSticks.Left.X * ushort.MaxValue);
+        }
+    }
+}
